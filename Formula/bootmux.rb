fe5d00eed83c10aa8abf027aae8e975c96bb164b
Griffin Qiu <griffinqiu@gmail.com>
2026-07-30 10:33:36 +0800

    bootmux 0.1.0

---
+++ Formula/bootmux.rb
@@ -0,0 +1,37 @@
+class Bootmux < Formula
+  desc "Manage tmux sessions and Herdr workspaces from tmuxinator-compatible YAML"
+  homepage "https://github.com/griffinqiu/bootmux"
+  url "https://github.com/griffinqiu/bootmux/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "689e6aa48cef049efe0b1bc5c0d8b78379f078b4315821c161495d1d4770c997"
+  license "MIT"
+  head "https://github.com/griffinqiu/bootmux.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    bash_completion.install "completion/bootmux.bash" => "bootmux"
+    zsh_completion.install "completion/bootmux.zsh" => "_bootmux"
+    fish_completion.install "completion/bootmux.fish"
+  end
+
+  test do
+    config = testpath/"brew-test.yml"
+    config.write <<~YAML
+      name: homebrew-test
+      root: "#{testpath}"
+      attach: false
+      windows:
+        - shell: echo homebrew
+    YAML
+
+    output = shell_output(
+      "#{bin}/bootmux --backend herdr debug --project-config #{config}",
+    )
+    assert_match "backend: herdr", output
+    assert_match "project: homebrew-test", output
+    assert_match "pane[0] commands=1", output
+    assert_match "bootmux #{version}", shell_output("#{bin}/bootmux --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# Griffin Qiu's Homebrew tap
+
+## bootmux
+
+Install [bootmux](https://github.com/griffinqiu/bootmux):
+
+```sh
+brew install griffinqiu/tap/bootmux
+```
+
+Upgrade or uninstall:
+
+```sh
+brew upgrade griffinqiu/tap/bootmux
+brew uninstall griffinqiu/tap/bootmux
+```
+
+The formula builds the tagged release from source and installs Bash, Zsh, and
+Fish completions. bootmux requires tmux 2.6+, Herdr 0.7.5/protocol 17, or both
+at runtime.
