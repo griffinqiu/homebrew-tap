7d9832022db4fd7849b1233be9d043acebe066a6
Griffin Qiu <griffinqiu@gmail.com>
2026-07-31 11:57:12 +0800

    bootmux 0.1.2

--- Formula/bootmux.rb
+++ Formula/bootmux.rb
@@ -1,8 +1,8 @@
 class Bootmux < Formula
-  desc "Manage tmux sessions and Herdr workspaces from tmuxinator-compatible YAML"
+  desc "Run tmuxinator-style YAML projects in tmux, Herdr, or zellij"
   homepage "https://github.com/griffinqiu/bootmux"
-  url "https://github.com/griffinqiu/bootmux/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "7f91ed3348e3bc3bc78a2afcbc9e7102177948b8b85c48aa770ce5303576b714"
+  url "https://github.com/griffinqiu/bootmux/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "df37dc6041f367c7a81e714a425f9952f18649129fde48be86a0b3c94a26376d"
   license "MIT"
   head "https://github.com/griffinqiu/bootmux.git", branch: "main"
 
@@ -32,6 +32,14 @@ class Bootmux < Formula
     assert_match "backend: herdr", output
     assert_match "project: homebrew-test", output
     assert_match "pane[0] commands=1", output
+
+    zellij_output = shell_output(
+      "#{bin}/bootmux --backend zellij debug --project-config #{config}",
+    )
+    assert_match "backend: zellij", zellij_output
+    assert_match "session: homebrew-test", zellij_output
+    assert_match "tab name=\"shell\"", zellij_output
+
     version_output = shell_output("#{bin}/bootmux --version")
     if build.stable?
       assert_match "bootmux #{version}", version_output
